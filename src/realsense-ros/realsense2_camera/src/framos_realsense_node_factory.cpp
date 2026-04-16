9d906a349d793ce26fcfab0b5e0139a4b781f1fe
TimR643 <tim@ingruk.de>
2026-04-16 14:17:16 +0200

    Disable FRAMOS d400e build by default and gate includes by flag

--- src/realsense-ros/realsense2_camera/src/framos_realsense_node_factory.cpp
+++ src/realsense-ros/realsense2_camera/src/framos_realsense_node_factory.cpp
@@ -13,7 +13,7 @@
 #include <sys/time.h>
 #include <regex>
 
-#if __has_include(<librealsense2/hpp/rs_d400e.hpp>)
+#if defined(RS2_USE_D400E_EXTENSIONS)
 #include <librealsense2/hpp/rs_d400e.hpp>
 #define HAS_RS2_D400E 1
 #else
